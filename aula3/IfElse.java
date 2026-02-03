c481758096ed72604fde2b57e91634ed50fc7ba1
Vickky <liviav987@gmail.com>
2026-02-03 10:23:53 -0300

    Add files via upload
    aula3 update - IfElse

--- aula3/IfElse.java
+++ aula3/IfElse.java
@@ -17,26 +17,22 @@ public class IfElse {
 
         IO.print("Digite sua nota");
         int nota1 = sc.nextInt();
-                if(nota1 < 4){
-                    IO.println("Reprovado");
-                } else {
-                    IO.println("Aprovado");
-                }
+
         IO.print("Digite sua nota");
         int nota2 = sc.nextInt();
-                if (nota2 <= 6){
+
+        IO.print("Digite sua nota");
+        int nota3 = sc.nextInt();
+
+        double media =(nota1 + nota2 + nota3) / 3;
+                System.out.println("A média aritmética é:" +media);
+                if (media < 4){
+                    IO.println("Reprovado");
+                }
+                else if (media <= 6) {
                     IO.println("Recuperação");
                 } else {
                     IO.println("Aprovado");
                 }
-        IO.print("Digite sua nota");
-        int nota3 = sc.nextInt();
-                if (nota3 > 6){
-                    IO.println("Aprovado");
-                } else {
-                    IO.println("Reprovado");
-                }
-                double media =(nota1 + nota2 + nota3) / 3;
-                System.out.println("A média aritmética é:" +media);
     }
 }
