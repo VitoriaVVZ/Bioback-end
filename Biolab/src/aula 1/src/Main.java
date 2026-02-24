9e75f979494804ffbb220280e2ddc87ead7fec4d
Vickky <liviav987@gmail.com>
2026-02-24 10:01:25 -0300

    Add files via upload

---
+++ Biolab/src/aula 1/src/Main.java
@@ -0,0 +1,44 @@
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//// void main() {
+//    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//    // to see how IntelliJ IDEA suggests fixing it.
+// IO.println(String.format("Hello World!"));
+//
+//    System.out.println("Hello World!");
+//
+//    //int valor = 8000;
+//
+//    //int numero = 25;
+//
+//    //float dadosFloat = 2.5f;
+//
+//    //float desenhoCanvas = 18.5f;
+//
+//    //char input = 'P';
+//    //char sexo = 'F';
+//
+//
+////CALCULOS ARITMÉTICOS
+//
+//    int one = 2 * 6 / 3;
+//
+//    int two = 3 + 2 * 4;
+//
+//    int three = (3 + 2) * 4;
+//
+//    int four = 60 / (3 + 2) * 4;
+//
+//    int five = 60 / ((3 + 2) * 4);
+//
+//    System.out.print(one);
+//
+//    System.out.print(two);
+//
+//    System.out.print(three);
+//
+//    System.out.print(four);
+//
+//    System.out.print(five);
+//}
+//
