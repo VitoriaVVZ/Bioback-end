deb26eee7d78d4e2657ed8a4db4be8fe8106b834
Vickky <liviav987@gmail.com>
2026-04-02 08:45:25 -0300

    Update PJ.java

--- Heranca/abstracao/src/PJ.java
+++ Heranca/abstracao/src/PJ.java
@@ -1,5 +1,13 @@
 // Conta Pessoa Jurídica
 public class PJ extends Conta{
+   
+        public PJ() {
+    }
+
+    public PJ(int numero, int agencia, double saldo, String titular) {
+        super(numero, agencia, saldo, titular);
+    }
+    
     @Override
     public void calcularTarifaMensal(){
         setSaldo(getSaldo()-20);
