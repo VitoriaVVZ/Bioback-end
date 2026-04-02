b985119557c55645b8299d5548e75601124aee03
Vickky <liviav987@gmail.com>
2026-04-02 08:41:52 -0300

    Update PJ.java
    Updated the PJ class to modify the monthly fee calculation and remove the override for the withdraw method.

--- Heranca/abstracao/src/PJ.java
+++ Heranca/abstracao/src/PJ.java
@@ -1,18 +1,9 @@
 // Conta Pessoa Jurídica
-public class PJ extends Conta {
-
-    public PJ(int numero, String agencia, String titular, double saldo) {
-        super(numero, agencia, titular, saldo);
-    }
-
+public class PJ extends Conta{
     @Override
-    public double calcularTarifaMensal() {
-        return 20.0;
-    }
-
-    @Override
-    public void sacar(double valor) {
-        super.setSaldo(getSaldo() - valor - 1.50);
-        super.setSaldo(super.getSaldo() - 20.0);
+    public void calcularTarifaMensal(){
+        setSaldo(getSaldo()-20);
+        System.out.println("Houve um desconto de R$20 da tarifa");
+        exibirSaldo();
     }
 }
