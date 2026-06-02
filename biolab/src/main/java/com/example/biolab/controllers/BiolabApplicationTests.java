bf17f1bfa1912408f1d1c45bb40d79ddcf27af11
Vickky <liviav987@gmail.com>
2026-06-02 08:27:38 -0300

    Spring Boot

---
+++ biolab/src/main/java/com/example/biolab/controllers/BiolabApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.biolab.controllers;
+
+import com.example.biolab.BiolabApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiolabApplicationTests{
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiolabApplication.class, args);
+	}
+
+}
